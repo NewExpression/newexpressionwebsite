7af66a4091b2d4c458380573916977ab68e9fbff
Jackey <jziwenchen@gmail.com>
2014-09-17 14:26:09 +0800

    更新快递查询友情链接

--- scripts/controller.js
+++ scripts/controller.js
@@ -96,16 +96,28 @@ angular.module("BonsdevApp.controllers", ["BonesApp.Directives"])
                 }];
             
                 $scope.services = [{
-                        thumbnail: "/misc/imgs/service1.png",
-                        title: "网站创意设计和开发",
-                        info: "网站创意带给用户以遐想, 设计带给用户便捷, 网站的架构和实现为客户的设想和市场计划提供强大的动力. 我们在网站方面积累了大量的经验, 包括Mobile App 设计和开发, 响应式网站设计和开发. 如果您有任何关于网站的需求和疑问, 可以向我们免费询问."
-                },{
                         thumbnail: "/misc/imgs/service2.png",
+                        techKeyword: "IOS, Android, App 设计, 智能应用",
                         title: "手机移动端开发",
                         info: "手机移动端作为最具有潮流和最具有改变现在互联网格局的平台, 我们为客户精心提供了定制各类手机应用的开发服务, 请告诉我们您的诉求, 我们将竭力服务让移动应用搭载您的产品启航在未来智能手机大海中."
                 },{
                         thumbnail: "/misc/imgs/service3.png",
+                        techKeyword: "Weibo, 微信公众平台, 微博企业应用, 社交推广, SEO",
                         title: "品牌推广和社交营销",
                         info: "近几年社交平台呈现百花齐放的姿态包括微博,腾讯微博,开心网,人人社交,微信等等, 公司因此为客户提供社交平台集成服务, 通过集成把您的产品推广到各个平台去,利用粉丝营销的技术提高用户黏度, 扩大市场和提高产品口碑."
+                },  {
+                    thumbnail: "",
+                    title: "电子商务网站建设",
+                    info: "我们基于全球最大的开源电子商城系统 Magento为企业提供优质的电子商务网站建设业务，系统功能包括订单，产品分类，多币种管理，物流服务管理等功能.  不管您是服装厂家还是运动鞋厂家，甚至是酒店企业，都可以选择我们的电子商务网站建设方案；灵活和强大是我们的电子商务网站的服务优势。",
+                    techKeyword: "PHP, CSS, HTML5, JAVASCRIPT, Magento, MySQL",
+                },  {
+                    thumbnail: "/misc/imgs/service1",
+                    title: "网站建设",
+                    info: "公司内部通过研发Fly CMS 内容管理系统 为企业网站建设提供优质的解决方案，不管是网站设计阶段，还是网站研发阶段，整个网站研发过程我们都会投入百分之一百的精力让您的网站与众不同和出彩。我们还可以为客户开发基于 Drupal 的内容网站，这样不管您网站有多复杂，内容多样，我们都可以让您应付自如.",
+                    techKeyword: "PHP, CSS3, HTML5, Angular.js , MySQL, Drupal, Yii Framework",
+                }, {
+                    thumbnail: "",
+                    title: "创意设计",
+                    info: "我们的设计师总是能在各种细节中找到创意灵感，设计总是简洁，大气.  我们不喜欢复杂，我们崇尚简单，我们相信简单需要智慧.创意设计服务包括企业AI, 企业Logo, 创意网站设计, 企业海报设计，企业日历设计，企业T-shirt设计.",
                 }];
 	});
